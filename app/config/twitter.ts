20443a95834f0ba0eede4daa8d95315ddca71922
SHIVAM SINGH <166604471+shivamsingh2609@users.noreply.github.com>
2025-02-25 15:58:57 +0530

    change

--- app/config/twitter.ts
+++ app/config/twitter.ts
@@ -1,6 +1,6 @@
 export const TWITTER_CONFIG = {
-  clientId: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID || "",
-  clientSecret: process.env.TWITTER_CLIENT_SECRET || "",
+  clientId: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID || "N2pwM2w3X3ZzUVhrNU5XeWk4WVI6MTpjaQ",
+  clientSecret: process.env.TWITTER_CLIENT_SECRET || "xWWn4A3ClWUq1Qe1qygpaTtjS-QLMiJrXPBelh9xKUzyz1mtBG",
   callbackUrl:
     process.env.NEXT_PUBLIC_TWITTER_CALLBACK_URL ||
     "https://z1-test-image-sizer.vercel.app/api/auth/twitter/callback",
